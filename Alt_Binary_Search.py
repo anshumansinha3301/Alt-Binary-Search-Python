843f24056a8ed3158dab1e8084ffa4cf669a076b
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-05 16:02:49 +0530

    Add files via upload

---
+++ Alt_Binary_Search.py
@@ -0,0 +1,15 @@
+
+def bsearch_alt(target, arr):
+    n = len(arr)
+    k = 0
+    i = n // 2
+    while (i >= 1):
+        while (k + i < n) and (arr[k + i] <= target):
+            k = k + 1
+        i = i // 2
+    
+    return k if arr[k] == target else -1
+
+
+
+print(bsearch_alt(4, [1, 2, 3, 4, 5]))
